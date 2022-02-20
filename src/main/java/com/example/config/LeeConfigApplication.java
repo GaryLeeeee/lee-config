23ffbc9d50a902eb8ce6f780315458504c164777
GaryLeeeee <664306561@qq.com>
2022-02-20 17:31:18 +0800

    init

---
+++ src/main/java/com/example/config/LeeConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeeConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeeConfigApplication.class, args);
+	}
+
+}
